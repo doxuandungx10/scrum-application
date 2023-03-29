3024d5865bdd48d6ac129f924a93c7bbf7ad5bfc
Do Xuan Dung <dungdx@pmr.vn>
2023-03-29 17:41:39 +0700

    ++ change theme

--- src/app/elements/header/header.component.ts
+++ src/app/elements/header/header.component.ts
@@ -1,27 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {NgbDropdownConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-	
-	toggleChat: boolean = true;
-	toggleSingle: boolean = true;
-	
-	constructor() { }
-	
-	ngOnInit(): void {
-	}
-	
-	
-	togglechatbar() {
-		this.toggleChat = !this.toggleChat;
-	}
-	singleChatWindow() {
-		this.toggleSingle = !this.toggleSingle;
-	}
+  toggleChat: boolean = true;
+  toggleSingle: boolean = true;
+  defaultLightMode: boolean = true;
+  @Output() newEvent = new EventEmitter<string>();
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  togglechatbar() {
+    this.toggleChat = !this.toggleChat;
+  }
+  singleChatWindow() {
+    this.toggleSingle = !this.toggleSingle;
+  }
+  toggle(event: any) {
+    console.log(event);
+  }
+  setTheme(value: string) {
+	this.newEvent.emit(value);
+  }
 }
