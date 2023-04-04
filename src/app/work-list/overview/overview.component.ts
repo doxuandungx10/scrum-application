279da3b5929b3507ed8bfe6821ad689f438c17b7
Do Xuan Dung <dungdx@pmr.vn>
2023-04-04 16:28:58 +0700

    +[Overview] add user; install fontawesome

--- src/app/work-list/overview/overview.component.ts
+++ src/app/work-list/overview/overview.component.ts
@@ -1,7 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProjectService } from 'src/app/services/project.service';
 import { UserService } from 'src/app/services/user.service';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { Observable, Subject, merge } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+} from 'rxjs/operators';
+import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-overview',
@@ -15,18 +25,24 @@ export class OverviewComponent implements OnInit {
   listUser: any[] = [];
   listAllUserSystem: any[] = [];
   initialProject: any;
-  
+  listSelectedUserId: any;
+
+  @ViewChild('instance', { static: true }) instance!: NgbTypeahead;
+  focus$ = new Subject<string>();
+  click$ = new Subject<string>();
+
   constructor(
     private router: ActivatedRoute,
     private userService: UserService,
-    private projectService: ProjectService
+    private projectService: ProjectService,
+    private modalService: NgbModal
   ) {
     // this.router.parent.snapshot.paramMap((params) => {
     //   console.log(params.id);
     // });
   }
-  
-  ngOnInit(){
+
+  ngOnInit() {
     this.getProjectInfo();
     this.getListUserByProjectId();
     this.getListUser();
@@ -49,8 +65,43 @@ export class OverviewComponent implements OnInit {
   getProjectInfo() {
     this.projectService.getProjectById(this.projectId).subscribe((res) => {
       this.project = res;
-      console.log("dung", res);
+      console.log('dung', res);
       this.initialProject = this.project;
     });
+  }
+
+  open(content: any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
+  }
+
+  search = (text$: Observable<string>) => {
+    const debouncedText$ = text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged()
+    );
+    const clicksWithClosedPopup$ = this.click$.pipe(
+      filter(() => !this.instance.isPopupOpen())
+    );
+    const inputFocus$ = this.focus$;
+
+    return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+      map((term) =>
+        (term === ''
+          ? this.listAllUserSystem
+          : this.listAllUserSystem.filter(
+              (v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1
+            )
+        ).slice(0, 10)
+      )
+    );
+  };
+
+  addUser() {
+    console.log('this.listSelectedUserId', this.listSelectedUserId);
+    this.userService
+      .addMemberToProject(this.listSelectedUserId, this.projectId)
+      .subscribe((res) => {
+        this.getListUserByProjectId();
+      });
   }
 }
