2b95f7b2130a89edd633f46ea1aa251dcede76e4
Do Xuan Dung <dungdx@pmr.vn>
2023-08-11 15:40:57 +0700

    + Quên mật khẩu

--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
+  step: number = 1;
+  email: string = "";
+  errorMessage: string = "";
+  formChangePassword: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  validateEmail(email: string) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  sendEmail() {
+    if (this.email.trim() == "") {
+      this.errorMessage = "Email không được để trống!";
+      return;
+    }
+    if (!this.validateEmail(this.email)) {
+      this.errorMessage = "Email không hợp lệ!";
+      return;
+    }
+    this.userService.sendEmailToChangePass(this.email).subscribe(res=>{
+      if(res.isValid){
+        this.step = 2;
+      }
+      else{
+        this.errorMessage = res.errorMessage; 
+      }
+    })
+  }
+
+  redirectLogin(){
+    this.router.navigate(['/page-login'])
+  }
 }
